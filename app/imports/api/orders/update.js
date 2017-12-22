023593687d27b4d0b75a251ee052a218762c3976
David Morrison <jdmorriso@gmail.com>
2017-12-23 00:00:57 +1100

    fix(orderNo): ensure check of orderNo is on a singleton

--- app/imports/api/orders/update.js
+++ app/imports/api/orders/update.js
@@ -4,12 +4,12 @@ import { orderCollection } from './model.js';
 
 
 export function update(newDoc) {
-	check(newDoc.orderNo, Number);
-
 	// handle updating an array of docs
 	if (_.isArray(newDoc)) {
 		return _.each(newDoc, d => update(d));
 	}
+
+	check(newDoc.orderNo, Number);
 
 	// try and find any existing order matching it
 	const oldDoc = orderCollection.findOne({ orderNo: newDoc.orderNo });
