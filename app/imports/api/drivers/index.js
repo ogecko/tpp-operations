30d0550460d3711890a3298d86f7b0619828da42
David Morrison <jdmorriso@gmail.com>
2018-10-18 09:44:10 +1100

    enh(drivers): update driver names

--- app/imports/api/drivers/index.js
+++ app/imports/api/drivers/index.js
@@ -1,12 +1,12 @@
 const driverCollection = [
-	{ seq: 1, name: 'Geoffrey', color: '#ff424c' },		// red
+	{ seq: 1, name: 'Kate', color: '#ff424c' },			// red
 	{ seq: 2, name: 'Arvin', color: '#3d29f6' },		// prusian blue
 	{ seq: 3, name: 'Mel', color: '#f47021' },			// orange
 	{ seq: 4, name: 'Phil', color: '#48a63a' },			// green
 	{ seq: 5, name: 'Carolyne', color: '#985236' },		// brown
 	{ seq: 6, name: 'Driver 6', color: '#a746ab' },		// purple
-	{ seq: 7, name: 'Driver 7', color: '#ffba22' },		// yellow
-	{ seq: 8, name: 'Driver 8', color: '#008dcf' },		// cyan
+	{ seq: 7, name: 'Joe', color: '#ffba22' },			// yellow
+	{ seq: 8, name: 'Michael', color: '#008dcf' },		// cyan
 ];
 
 export const drivers = {
