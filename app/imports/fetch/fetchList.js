d1d9671044672a4e02da358e08d48d8e65b9ac37
David Morrison <jdmorriso@gmail.com>
2018-11-10 14:47:01 +1100

    fix(fetch): remove fetchlist call to Rocketspark

--- app/imports/fetch/fetchList.js
+++ app/imports/fetch/fetchList.js
@@ -12,29 +12,31 @@ export function fetchList(job, cb) {
 	console.log('Calling fetchList');
 	if (Meteor.isServer) {
 		odooFetch();
-		const web = Nightmare({ pollInterval: 50 })			// { show: true, pollInterval: 50 }
-		.use(loginRocketSpark('/dashboard/shop_settings/orders'))
-		.wait('#orders-vue > div > table')
-		.evaluate(function () {
-			return document.querySelector('body').outerHTML;
-		})
-		.end()
-		.then(function (body) {
-			const result = parse.html(body, fetchListSpec);
-			// console.log('list parse', JSON.stringify(result, undefined, 2));
-			orders.update(result.data.orders);
-			console.log(`Received (${result.data.orders.length} orders)`);
-			_.each(result.data.orders, order => {
-				if (!orders.isFetched(order.orderNo)) {
-					Meteor.call('fetch order', order.orderNo);
-				}
-			});
-			job.done(); cb();
-		})
-		.catch(function (error) {
-			console.log('error from fetchlist', error);
-			job.fail(); cb();
-		});
+
+		// Rocketspark Fetch		
+		// const web = Nightmare({ pollInterval: 50 })			// { show: true, pollInterval: 50 }
+		// .use(loginRocketSpark('/dashboard/shop_settings/orders'))
+		// .wait('#orders-vue > div > table')
+		// .evaluate(function () {
+		// 	return document.querySelector('body').outerHTML;
+		// })
+		// .end()
+		// .then(function (body) {
+		// 	const result = parse.html(body, fetchListSpec);
+		// 	// console.log('list parse', JSON.stringify(result, undefined, 2));
+		// 	orders.update(result.data.orders);
+		// 	console.log(`Received (${result.data.orders.length} orders)`);
+		// 	_.each(result.data.orders, order => {
+		// 		if (!orders.isFetched(order.orderNo)) {
+		// 			Meteor.call('fetch order', order.orderNo);
+		// 		}
+		// 	});
+		// 	job.done(); cb();
+		// })
+		// .catch(function (error) {
+		// 	console.log('error from fetchlist', error);
+		// 	job.fail(); cb();
+		// });
 	}
 }
 
