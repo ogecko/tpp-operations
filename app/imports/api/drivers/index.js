817db98f682912a752ef24f405ed3faa1ff8d999
David Morrison <jdmorriso@gmail.com>
2020-04-29 19:58:57 +1000

    enh(drivers): add 8 more drivers

--- app/imports/api/drivers/index.js
+++ app/imports/api/drivers/index.js
@@ -1,12 +1,20 @@
 const driverCollection = [
-	{ seq: 1, name: 'Kate', color: '#ff424c' },			// red
-	{ seq: 2, name: 'Arvin', color: '#3d29f6' },		// prusian blue
-	{ seq: 3, name: 'Mel', color: '#f47021' },			// orange
-	{ seq: 4, name: 'Phil', color: '#48a63a' },			// green
-	{ seq: 5, name: 'Carolyne', color: '#985236' },		// brown
-	{ seq: 6, name: 'Driver 6', color: '#a746ab' },		// purple
-	{ seq: 7, name: 'Joe', color: '#ffba22' },			// yellow
-	{ seq: 8, name: 'Michael', color: '#008dcf' },		// cyan
+	{ seq: 01, name: 'Kate',     color: '#2E91E5' },		// blue
+	{ seq: 02, name: 'Arvin',    color: '#6C4516' },		// poo
+	{ seq: 03, name: 'Mel',      color: '#1CA71C' },		// green
+	{ seq: 04, name: 'Phil',     color: '#FB0D0D' },		// red
+	{ seq: 05, name: 'Raj', 	 color: '#DA16FF' },		// purple
+	{ seq: 06, name: 'Dave',     color: '#B68100' },		// tan
+	{ seq: 07, name: 'Joe',      color: '#750D86' },		// deep purple
+	{ seq: 08, name: 'Michael',  color: '#EB663B' },		// peach
+	{ seq: 09, name: 'Driver 1', color: '#00A08B' },		// cyan
+	{ seq: 10, name: 'Driver 2', color: '#FB00D1' },		// pink
+	{ seq: 11, name: 'Driver 3', color: '#B2828D' },		// tan
+	{ seq: 12, name: 'Driver 4', color: '#6C7C32' },		// green
+	{ seq: 13, name: 'Driver 5', color: '#778AAE' },		// grey
+	{ seq: 14, name: 'Driver 6', color: '#862A16' },		// brown
+	{ seq: 15, name: 'Driver 7', color: '#A777F1' },		// mauve
+	{ seq: 16, name: 'Driver 8', color: '#AF0038' },		// red
 ];
 
 export const drivers = {
